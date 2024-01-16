73d6b4d586f3f3bf8b3c54fb707f1cd2d0567240
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-01-16 12:06:21 -0700

    restarting project from the beginning after 6-8 week hiatus

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ class Config():
     '''
 
     FLASK_APP = os.getenv('FLASK_APP')
-    FLASK_ENV = os.getenv('FLASK_ENV')
+    # FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    # FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Winter23#'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
